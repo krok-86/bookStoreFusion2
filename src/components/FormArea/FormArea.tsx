7f391c1a2df4768c701587946262ab4bc72c53f9
krok-86 <8700478@mail.ru>
2024-02-13 16:49:53 +0300

    fix error in inputArea

---
+++ src/components/FormArea/FormArea.tsx
@@ -0,0 +1,31 @@
+// import { Button } from "@mui/material";
+// import { Stack } from "@mui/system";
+// import { TextField } from "material-ui";
+// import { useForm } from "react-hook-form";
+
+// type FormValues = {
+//     email: string;
+//     password: string;
+// }
+// export const FormArea = () => {
+//    const defaultValues={
+//         email: '',
+//         password: '',
+//     };
+//     const form = useForm<FormValues>({
+//         defaultValues;
+//     })
+//     return (
+//         <>
+//         <form>
+//         <Stack spacing={2} width={400}>
+//         <TextField label="Email" type="email" />
+//         <TextField label="Password" type="password" />
+//             <Button type="submit" variant="contained" color='primary'>
+//                 submit
+//             </Button>
+//         </Stack>
+//         </form>
+//         </>
+//     )
+// }
